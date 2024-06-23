550752f7a63cb12d657845022d510947b639a874
admin <admin@admins-MacBook-Pro.local>
2024-06-22 20:22:52 -0700

    i'm here

--- github/github/ContentView.swift
+++ github/github/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("this is github demo")
+            Text("hey i have collab")
         }
         .padding()
     }

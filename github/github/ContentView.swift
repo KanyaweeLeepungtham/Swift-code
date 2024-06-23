c67e31c8c67d3d676af871387ed680f80a5e2905
admin <admin@admins-MacBook-Pro.local>
2024-06-22 20:13:17 -0700

    add this github demo

--- github/github/ContentView.swift
+++ github/github/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                // .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("this is github demo")
         }
         .padding()
     }

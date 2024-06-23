f5553b754af4fb9480a60f30fa2caa6d7376d6e3
admin <admin@admins-MacBook-Pro.local>
2024-06-22 20:03:59 -0700

    first commit

---
+++ github/github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
